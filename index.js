3fb0db934b6f50c31dda4e48da8528e536ac7990
Vishwa Mihiranga <145928580+vishwamihiranga@users.noreply.github.com>
2024-11-29 16:36:08 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+const bodyParser = require('body-parser');
+const path = require('path');
+const server = require('./qr');
+const code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/qrweb',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+const app = express();
+const __path = process.cwd();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// Routes
+app.use('/qr', server);
+app.use('/code', code);
+app.use('/qrweb', (req, res) => {
+  res.sendFile(path.join(__path, 'qr.html'));
+});
+app.use('/pair', (req, res) => {
+  res.sendFile(path.join(__path, 'pair.html'));
+});
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__path, 'main.html'));
+});
+
+// Export the app as a module for Vercel
+module.exports = app;
